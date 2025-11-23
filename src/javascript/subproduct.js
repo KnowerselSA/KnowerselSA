ceb3016554a2ee2d38ee2fe4f38b23e5cc485956
Rohit Lahu Bhalekar <165887184+DevROHIT11@users.noreply.github.com>
2025-11-24 00:18:38 +0530

    mobile responsive

--- src/javascript/subproduct.js
+++ src/javascript/subproduct.js
@@ -1,43 +1,55 @@
-// slider 
-var swiper = new Swiper('.mySwiper' ,{
-    direction : "vertical" ,
-    effect : "fade" ,
-     pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-     
-      mousewheel :true
+// slider
+var swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  mousewheel: true,
 });
-// product page nav items 
-gsap.to(".list_item a" , {
-    color : "white" , 
-  scrollTrigger :{
-    trigger : ".page-container" ,
+// product page nav items
+gsap.to(".list_item a", {
+  color: "white",
+  scrollTrigger: {
+    trigger: ".page-container",
     // scroller: "body",
     toggleActions: "play none none reverse",
     // markers : true,
-    start : "top 10%"
-  }
-  
+    start: "top 10%",
+  },
+});
+
+// changing the menu img when it reach the slider
+gsap.to("menu-img", {
+  scrollTrigger: {
+    trigger: ".swiper-wrapper",
+    // markers : true,
+    start: "top 5%",
+    onEnter: () => {
+      document.getElementById("menu-img").src = "src/assets/menu2.png";
+    },
+    onLeaveBack: () => {
+      document.getElementById("menu-img").src = "src/assets/menu.PNG";
+    },
+  },
 });
 // const lenis = new Lenis({
 //   smooth: true,
 //   lerp: 0.05,   // default ~0.1
-//    wheelMultiplier: 0.5, 
+//    wheelMultiplier: 0.5,
 // });
 
 // gsap.registerPlugin(ScrollTrigger);
-
-
 
 // gsap.to('.page2' , {
 //     y : "-50vh",
 //     // duration : 2 ,
 //     opacity : 1 ,
 //     scrollTrigger:{
-//         trigger : ".page1   " , 
-//         scroller : "body" , 
+//         trigger : ".page1   " ,
+//         scroller : "body" ,
 //         markers : true ,
 //         start : "bottom 70%" ,
 //         end : "bottom 30%" ,
